c0dd85a1885aefc9b797fc0c6c8bf49f8357c4e5
Christian Legnitto <LegNeato@users.noreply.github.com>
2025-07-10 18:37:01 +0200

    Fix failing doctests (#235)

--- crates/cust_core/src/lib.rs
+++ crates/cust_core/src/lib.rs
@@ -18,9 +18,9 @@ pub mod _hidden {
     /// There are two ways to implement DeviceCopy on your type. The simplest is to use `derive`:
     ///
     /// ```
-    /// use cust::DeviceCopy;
+    /// use cust_core::DeviceCopy;
     ///
-    /// #[derive(Clone, DeviceCopy)]
+    /// #[derive(Clone, Copy, DeviceCopy)]
     /// struct MyStruct(u64);
     ///
     /// # fn main () {}
@@ -31,7 +31,7 @@ pub mod _hidden {
     /// be copied to the device:
     ///
     /// ```compile_fail
-    /// use cust::DeviceCopy;
+    /// use cust_core::DeviceCopy;
     ///
     /// #[derive(Clone, DeviceCopy)]
     /// struct MyStruct(Vec<u64>);
@@ -41,9 +41,9 @@ pub mod _hidden {
     /// You can also implement `DeviceCopy` unsafely:
     ///
     /// ```
-    /// use cust::memory::DeviceCopy;
+    /// use cust_core::DeviceCopy;
     ///
-    /// #[derive(Clone)]
+    /// #[derive(Clone, Copy)]
     /// struct MyStruct(u64);
     ///
     /// unsafe impl DeviceCopy for MyStruct { }
